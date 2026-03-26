4f19944a6daf5c1d2a28538e9f2c5037c1c73f15
Polartica <109315921+Polartica@users.noreply.github.com>
2026-03-26 15:35:48 -0300

    Admin update CMS data

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,89 +2,398 @@ window.crlvData = {
   "adm": {
     "titulo": "Administração",
     "itens": [
-      { "id": "IVI8685", "descricao": "Carro Administrativo - IVI8685", "imagem": "", "pdf": "./ADM/IVI8685.pdf" },
-      { "id": "IWR8351", "descricao": "Carro Administrativo - IWR8351", "imagem": "", "pdf": "./ADM/IWR8351.pdf" },
-      { "id": "JBX6E78", "descricao": "Carro Administrativo - JBX6E78", "imagem": "", "pdf": "./ADM/JBX6E78.pdf" }
+      {
+        "id": "IVI8685",
+        "descricao": "Carro Administrativo - IVI8685",
+        "imagem": "",
+        "pdf": "./ADM/IVI8685.pdf"
+      },
+      {
+        "id": "JBX6E78",
+        "descricao": "Carro Administrativo - JBX6E78",
+        "imagem": "",
+        "pdf": "./ADM/JBX6E78.pdf"
+      }
     ]
   },
   "entrega": {
     "titulo": "Entrega",
     "itens": [
-      { "id": "FIX0I92", "descricao": "Caminhão FIX0I92", "imagem": "", "pdf": "./Entrega/FIX0I92.pdf" },
-      { "id": "FQH8C04", "descricao": "Caminhão FQH8C04", "imagem": "", "pdf": "./Entrega/FQH8C04.pdf" },
-      { "id": "FTW6H54", "descricao": "Caminhão FTW6H54", "imagem": "", "pdf": "./Entrega/FTW6H54.pdf" },
-      { "id": "ILS3023", "descricao": "Caminhão ILS3023", "imagem": "", "pdf": "./Entrega/ILS3023.pdf" },
-      { "id": "IOG3B06", "descricao": "Caminhão IOG3B06", "imagem": "", "pdf": "./Entrega/IOG3B06.pdf" },
-      { "id": "IQU0163", "descricao": "Caminhão IQU0163", "imagem": "", "pdf": "./Entrega/IQU0163.pdf" },
-      { "id": "IRC3J70", "descricao": "Caminhão IRC3J70", "imagem": "", "pdf": "./Entrega/IRC3J70.pdf" },
-      { "id": "ISS6574", "descricao": "Caminhão ISS6574", "imagem": "", "pdf": "./Entrega/ISS6574.pdf" },
-      { "id": "IWR8351", "descricao": "Caminhão IWR8351", "imagem": "", "pdf": "./Entrega/IWR8351.pdf" },
-      { "id": "JBV5I06", "descricao": "Caminhão JBV5I06", "imagem": "", "pdf": "./Entrega/JBV5I06.pdf" },
-      { "id": "JCK1H24", "descricao": "Caminhão JCK1H24", "imagem": "", "pdf": "./Entrega/JCK1H24.pdf" },
-      { "id": "JDL7D19", "descricao": "Caminhão JDL7D19", "imagem": "", "pdf": "./Entrega/JDL7D19.pdf" },
-      { "id": "JDL7D21", "descricao": "Caminhão JDL7D21", "imagem": "", "pdf": "./Entrega/JDL7D21.pdf" },
-      { "id": "KXN5871", "descricao": "Caminhão KXN5871", "imagem": "", "pdf": "./Entrega/KXN5871.pdf" },
-      { "id": "KZJ0615", "descricao": "Caminhão KZJ0615", "imagem": "", "pdf": "./Entrega/KZJ0615.pdf" },
-      { "id": "KZP5142", "descricao": "Caminhão KZP5142", "imagem": "", "pdf": "./Entrega/KZP5142.pdf" },
-      { "id": "LOZ9046", "descricao": "Caminhão LOZ9046", "imagem": "", "pdf": "./Entrega/LOZ9046.pdf" },
-      { "id": "MCV8E33", "descricao": "Caminhão MCV8E33", "imagem": "", "pdf": "./Entrega/MCV8E33.pdf" },
-      { "id": "OZT4F94", "descricao": "Caminhão OZT4F94", "imagem": "", "pdf": "./Entrega/OZT4F94.pdf" },
-      { "id": "TQW5A44", "descricao": "Caminhão TQW5A44", "imagem": "", "pdf": "./Entrega/TQW5A44.pdf" }
+      {
+        "id": "FIX0I92",
+        "descricao": "Caminhão FIX0I92",
+        "imagem": "",
+        "pdf": "./Entrega/FIX0I92.pdf"
+      },
+      {
+        "id": "FQH8C04",
+        "descricao": "Caminhão FQH8C04",
+        "imagem": "",
+        "pdf": "./Entrega/FQH8C04.pdf"
+      },
+      {
+        "id": "FTW6H54",
+        "descricao": "Caminhão FTW6H54",
+        "imagem": "",
+        "pdf": "./Entrega/FTW6H54.pdf"
+      },
+      {
+        "id": "ILS3023",
+        "descricao": "Caminhão ILS3023",
+        "imagem": "",
+        "pdf": "./Entrega/ILS3023.pdf"
+      },
+      {
+        "id": "IOG3B06",
+        "descricao": "Caminhão IOG3B06",
+        "imagem": "",
+        "pdf": "./Entrega/IOG3B06.pdf"
+      },
+      {
+        "id": "IQU0163",
+        "descricao": "Caminhão IQU0163",
+        "imagem": "",
+        "pdf": "./Entrega/IQU0163.pdf"
+      },
+      {
+        "id": "IRC3J70",
+        "descricao": "Caminhão IRC3J70",
+        "imagem": "",
+        "pdf": "./Entrega/IRC3J70.pdf"
+      },
+      {
+        "id": "ISS6574",
+        "descricao": "Caminhão ISS6574",
+        "imagem": "",
+        "pdf": "./Entrega/ISS6574.pdf"
+      },
+      {
+        "id": "IWR8351",
+        "descricao": "Caminhão IWR8351",
+        "imagem": "",
+        "pdf": "./Entrega/IWR8351.pdf"
+      },
+      {
+        "id": "JBV5I06",
+        "descricao": "Caminhão JBV5I06",
+        "imagem": "",
+        "pdf": "./Entrega/JBV5I06.pdf"
+      },
+      {
+        "id": "JCK1H24",
+        "descricao": "Caminhão JCK1H24",
+        "imagem": "",
+        "pdf": "./Entrega/JCK1H24.pdf"
+      },
+      {
+        "id": "JDL7D19",
+        "descricao": "Caminhão JDL7D19",
+        "imagem": "",
+        "pdf": "./Entrega/JDL7D19.pdf"
+      },
+      {
+        "id": "JDL7D21",
+        "descricao": "Caminhão JDL7D21",
+        "imagem": "",
+        "pdf": "./Entrega/JDL7D21.pdf"
+      },
+      {
+        "id": "KXN5871",
+        "descricao": "Caminhão KXN5871",
+        "imagem": "",
+        "pdf": "./Entrega/KXN5871.pdf"
+      },
+      {
+        "id": "KZJ0615",
+        "descricao": "Caminhão KZJ0615",
+        "imagem": "",
+        "pdf": "./Entrega/KZJ0615.pdf"
+      },
+      {
+        "id": "KZP5142",
+        "descricao": "Caminhão KZP5142",
+        "imagem": "",
+        "pdf": "./Entrega/KZP5142.pdf"
+      },
+      {
+        "id": "LOZ9046",
+        "descricao": "Caminhão LOZ9046",
+        "imagem": "",
+        "pdf": "./Entrega/LOZ9046.pdf"
+      },
+      {
+        "id": "MCV8E33",
+        "descricao": "Caminhão MCV8E33",
+        "imagem": "",
+        "pdf": "./Entrega/MCV8E33.pdf"
+      },
+      {
+        "id": "OZT4F94",
+        "descricao": "Caminhão OZT4F94",
+        "imagem": "",
+        "pdf": "./Entrega/OZT4F94.pdf"
+      },
+      {
+        "id": "TQW5A44",
+        "descricao": "Caminhão TQW5A44",
+        "imagem": "",
+        "pdf": "./Entrega/TQW5A44.pdf"
+      }
     ]
   },
   "puxada": {
     "titulo": "Puxada",
     "itens": [
-      { "id": "IHZ8169", "descricao": "Caminhão IHZ8169", "imagem": "", "pdf": "./Puxada/IHZ8169.pdf" },
-      { "id": "IOS5240", "descricao": "Caminhão IOS5240", "imagem": "", "pdf": "./Puxada/IOS5240.pdf" },
-      { "id": "IPU3079", "descricao": "Caminhão IPU3079", "imagem": "", "pdf": "./Puxada/IPU3079.pdf" },
-      { "id": "IPU3A79", "descricao": "Caminhão IPU3A79", "imagem": "", "pdf": "./Puxada/IPU3A79.pdf" },
-      { "id": "IZH3E91", "descricao": "Caminhão IZH3E91", "imagem": "", "pdf": "./Puxada/IZH3E91.pdf" },
-      { "id": "JBA3A65", "descricao": "Caminhão JBA3A65", "imagem": "", "pdf": "./Puxada/JBA3A65.pdf" },
-      { "id": "JBG6D72", "descricao": "Caminhão JBG6D72", "imagem": "", "pdf": "./Puxada/JBG6D72.pdf" },
-      { "id": "JCP5J59", "descricao": "Caminhão JCP5J59", "imagem": "", "pdf": "./Puxada/JCP5J59.pdf" }
+      {
+        "id": "IHZ8169",
+        "descricao": "Caminhão IHZ8169",
+        "imagem": "",
+        "pdf": "./Puxada/IHZ8169.pdf"
+      },
+      {
+        "id": "IOS5240",
+        "descricao": "Caminhão IOS5240",
+        "imagem": "",
+        "pdf": "./Puxada/IOS5240.pdf"
+      },
+      {
+        "id": "IPU3079",
+        "descricao": "Caminhão IPU3079",
+        "imagem": "",
+        "pdf": "./Puxada/IPU3079.pdf"
+      },
+      {
+        "id": "IPU3A79",
+        "descricao": "Caminhão IPU3A79",
+        "imagem": "",
+        "pdf": "./Puxada/IPU3A79.pdf"
+      },
+      {
+        "id": "IZH3E91",
+        "descricao": "Caminhão IZH3E91",
+        "imagem": "",
+        "pdf": "./Puxada/IZH3E91.pdf"
+      },
+      {
+        "id": "JBA3A65",
+        "descricao": "Caminhão JBA3A65",
+        "imagem": "",
+        "pdf": "./Puxada/JBA3A65.pdf"
+      },
+      {
+        "id": "JBG6D72",
+        "descricao": "Caminhão JBG6D72",
+        "imagem": "",
+        "pdf": "./Puxada/JBG6D72.pdf"
+      },
+      {
+        "id": "JCP5J59",
+        "descricao": "Caminhão JCP5J59",
+        "imagem": "",
+        "pdf": "./Puxada/JCP5J59.pdf"
+      }
     ]
   },
   "vendas_carros": {
     "titulo": "Vendas - Carros",
     "itens": [
-      { "id": "IZE6H43", "descricao": "Carro IZE6H43", "imagem": "", "pdf": "./Vendas/Carros/IZE6H43.pdf" },
-      { "id": "IZE6H44", "descricao": "Carro IZE6H44", "imagem": "", "pdf": "./Vendas/Carros/IZE6H44.pdf" },
-      { "id": "IZE6I79", "descricao": "Carro IZE6I79", "imagem": "", "pdf": "./Vendas/Carros/IZE6I79.pdf" },
-      { "id": "IZG5F32", "descricao": "Carro IZG5F32", "imagem": "", "pdf": "./Vendas/Carros/IZG5F32.pdf" },
-      { "id": "JBO0E75", "descricao": "Carro JBO0E75", "imagem": "", "pdf": "./Vendas/Carros/JBO0E75.pdf" },
-      { "id": "JBD6C54", "descricao": "Carro JBD6C54", "imagem": "", "pdf": "./Vendas/Carros/JBD6C54.pdf" },
-      { "id": "TRC8G55", "descricao": "Carro TRC8G55", "imagem": "", "pdf": "./Vendas/Carros/TRC8G55.pdf" }
+      {
+        "id": "IZE6H43",
+        "descricao": "Carro IZE6H43",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/IZE6H43.pdf"
+      },
+      {
+        "id": "IZE6H44",
+        "descricao": "Carro IZE6H44",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/IZE6H44.pdf"
+      },
+      {
+        "id": "IZE6I79",
+        "descricao": "Carro IZE6I79",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/IZE6I79.pdf"
+      },
+      {
+        "id": "IZG5F32",
+        "descricao": "Carro IZG5F32",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/IZG5F32.pdf"
+      },
+      {
+        "id": "JBO0E75",
+        "descricao": "Carro JBO0E75",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/JBO0E75.pdf"
+      },
+      {
+        "id": "JBD6C54",
+        "descricao": "Carro JBD6C54",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/JBD6C54.pdf"
+      },
+      {
+        "id": "TRC8G55",
+        "descricao": "Carro TRC8G55",
+        "imagem": "",
+        "pdf": "./Vendas/Carros/TRC8G55.pdf"
+      }
     ]
   },
   "vendas_motos": {
     "titulo": "Vendas - Motos",
     "itens": [
-      { "id": "ISY7098", "descricao": "Moto ISY7098", "imagem": "", "pdf": "./Vendas/Motos/ISY7098.pdf" },
-      { "id": "ISY7132", "descricao": "Moto ISY7132", "imagem": "", "pdf": "./Vendas/Motos/ISY7132.pdf" },
-      { "id": "ISY7201", "descricao": "Moto ISY7201", "imagem": "", "pdf": "./Vendas/Motos/ISY7201.pdf" },
-      { "id": "ISY7262", "descricao": "Moto ISY7262", "imagem": "", "pdf": "./Vendas/Motos/ISY7262.pdf" },
-      { "id": "ISY7317", "descricao": "Moto ISY7317", "imagem": "", "pdf": "./Vendas/Motos/ISY7317.pdf" },
-      { "id": "ISY7352", "descricao": "Moto ISY7352", "imagem": "", "pdf": "./Vendas/Motos/ISY7352.pdf" },
-      { "id": "ISY7382", "descricao": "Moto ISY7382", "imagem": "", "pdf": "./Vendas/Motos/ISY7382.pdf" },
-      { "id": "ISY7445", "descricao": "Moto ISY7445", "imagem": "", "pdf": "./Vendas/Motos/ISY7445.pdf" },
-      { "id": "JAQ4E67", "descricao": "Moto JAQ4E67", "imagem": "", "pdf": "./Vendas/Motos/JAQ4E67.pdf" },
-      { "id": "JAQ4E72", "descricao": "Moto JAQ4E72", "imagem": "", "pdf": "./Vendas/Motos/JAQ4E72.pdf" },
-      { "id": "JAR2A62", "descricao": "Moto JAR2A62", "imagem": "", "pdf": "./Vendas/Motos/JAR2A62.pdf" },
-      { "id": "JAR2B48", "descricao": "Moto JAR2B48", "imagem": "", "pdf": "./Vendas/Motos/JAR2B48.pdf" },
-      { "id": "JAS7E80", "descricao": "Moto JAS7E80", "imagem": "", "pdf": "./Vendas/Motos/JAS7E80.pdf" },
-      { "id": "JAT7B31", "descricao": "Moto JAT7B31", "imagem": "", "pdf": "./Vendas/Motos/JAT7B31.pdf" },
-      { "id": "JAT7B34", "descricao": "Moto JAT7B34", "imagem": "", "pdf": "./Vendas/Motos/JAT7B34.pdf" },
-      { "id": "JAT8G56", "descricao": "Moto JAT8G56", "imagem": "", "pdf": "./Vendas/Motos/JAT8G56.pdf" },
-      { "id": "JAV6B81", "descricao": "Moto JAV6B81", "imagem": "", "pdf": "./Vendas/Motos/JAV6B81.pdf" },
-      { "id": "JAV9C46", "descricao": "Moto JAV9C46", "imagem": "", "pdf": "./Vendas/Motos/JAV9C46.pdf" },
-      { "id": "JAW6I65", "descricao": "Moto JAW6I65", "imagem": "", "pdf": "./Vendas/Motos/JAW6I65.pdf" },
-      { "id": "JAW9F11", "descricao": "Moto JAW9F11", "imagem": "", "pdf": "./Vendas/Motos/JAW9F11.pdf" },
-      { "id": "JAY9E36", "descricao": "Moto JAY9E36", "imagem": "", "pdf": "./Vendas/Motos/JAY9E36.pdf" },
-      { "id": "JBI6G83", "descricao": "Moto JBI6G83", "imagem": "", "pdf": "./Vendas/Motos/JBI6G83.pdf" },
-      { "id": "JBN8H83", "descricao": "Moto JBN8H83", "imagem": "", "pdf": "./Vendas/Motos/JBN8H83.pdf" },
-      { "id": "JBI6G07", "descricao": "Moto JBI6G07", "imagem": "", "pdf": "./Vendas/Motos/JBI6G07.pdf" },
-      { "id": "JBI6G69", "descricao": "Moto JBI6G69", "imagem": "", "pdf": "./Vendas/Motos/JBI6G69.pdf" }
+      {
+        "id": "ISY7098",
+        "descricao": "Moto ISY7098",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7098.pdf"
+      },
+      {
+        "id": "ISY7132",
+        "descricao": "Moto ISY7132",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7132.pdf"
+      },
+      {
+        "id": "ISY7201",
+        "descricao": "Moto ISY7201",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7201.pdf"
+      },
+      {
+        "id": "ISY7262",
+        "descricao": "Moto ISY7262",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7262.pdf"
+      },
+      {
+        "id": "ISY7317",
+        "descricao": "Moto ISY7317",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7317.pdf"
+      },
+      {
+        "id": "ISY7352",
+        "descricao": "Moto ISY7352",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7352.pdf"
+      },
+      {
+        "id": "ISY7382",
+        "descricao": "Moto ISY7382",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7382.pdf"
+      },
+      {
+        "id": "ISY7445",
+        "descricao": "Moto ISY7445",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/ISY7445.pdf"
+      },
+      {
+        "id": "JAQ4E67",
+        "descricao": "Moto JAQ4E67",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAQ4E67.pdf"
+      },
+      {
+        "id": "JAQ4E72",
+        "descricao": "Moto JAQ4E72",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAQ4E72.pdf"
+      },
+      {
+        "id": "JAR2A62",
+        "descricao": "Moto JAR2A62",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAR2A62.pdf"
+      },
+      {
+        "id": "JAR2B48",
+        "descricao": "Moto JAR2B48",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAR2B48.pdf"
+      },
+      {
+        "id": "JAS7E80",
+        "descricao": "Moto JAS7E80",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAS7E80.pdf"
+      },
+      {
+        "id": "JAT7B31",
+        "descricao": "Moto JAT7B31",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAT7B31.pdf"
+      },
+      {
+        "id": "JAT7B34",
+        "descricao": "Moto JAT7B34",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAT7B34.pdf"
+      },
+      {
+        "id": "JAT8G56",
+        "descricao": "Moto JAT8G56",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAT8G56.pdf"
+      },
+      {
+        "id": "JAV6B81",
+        "descricao": "Moto JAV6B81",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAV6B81.pdf"
+      },
+      {
+        "id": "JAV9C46",
+        "descricao": "Moto JAV9C46",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAV9C46.pdf"
+      },
+      {
+        "id": "JAW6I65",
+        "descricao": "Moto JAW6I65",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAW6I65.pdf"
+      },
+      {
+        "id": "JAW9F11",
+        "descricao": "Moto JAW9F11",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAW9F11.pdf"
+      },
+      {
+        "id": "JAY9E36",
+        "descricao": "Moto JAY9E36",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JAY9E36.pdf"
+      },
+      {
+        "id": "JBI6G83",
+        "descricao": "Moto JBI6G83",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JBI6G83.pdf"
+      },
+      {
+        "id": "JBN8H83",
+        "descricao": "Moto JBN8H83",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JBN8H83.pdf"
+      },
+      {
+        "id": "JBI6G07",
+        "descricao": "Moto JBI6G07",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JBI6G07.pdf"
+      },
+      {
+        "id": "JBI6G69",
+        "descricao": "Moto JBI6G69",
+        "imagem": "",
+        "pdf": "./Vendas/Motos/JBI6G69.pdf"
+      }
     ]
   }
 };
